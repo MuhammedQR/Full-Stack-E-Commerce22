381d86734c9d5a4afabd711820440957da3fc514
Muhammed22Qarwani <123984257+Muhammed22Qarwani@users.noreply.github.com>
2024-06-14 17:15:20 +0400

    update

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(
 );
 app.use(express.json()); // تحديد حد الحمولة لـ 10 ميجابايت
 app.use(cookieParser());
-app.use("api", router);
+app.use("/api", router);
 
 const PORT = 8080 || process.env.PORT;
 connectDB().then(() => {
