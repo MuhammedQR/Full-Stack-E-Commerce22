d680c4e005e4e927ba6a34fc1b9010612af208e6
Muhammed22Qarwani <123984257+Muhammed22Qarwani@users.noreply.github.com>
2024-06-14 18:04:38 +0400

    update

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,6 @@ app.use("/api", router);
 const PORT = 8080 || process.env.PORT;
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log("connect to DB");
     console.log("server is runing");
   });
 });
